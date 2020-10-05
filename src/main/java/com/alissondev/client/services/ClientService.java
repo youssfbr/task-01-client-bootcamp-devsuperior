209c828f39d4a9be185addac61b3b0f328de9206
alisson youssf <youssfbr@gmail.com>
2020-10-05 17:55:25 -0300

    Delete Client

--- src/main/java/com/alissondev/client/services/ClientService.java
+++ src/main/java/com/alissondev/client/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alissondev.client.dto.ClientDTO;
 import com.alissondev.client.entities.Client;
 import com.alissondev.client.repositories.ClientRepository;
+import com.alissondev.client.services.exceptions.DatabaseException;
 import com.alissondev.client.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -64,6 +67,19 @@ public class ClientService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);	
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+				
 	}
 	
 	
