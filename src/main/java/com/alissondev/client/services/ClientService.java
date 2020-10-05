e6c47462541092bf56366f626bfe3538af2e2073
alisson youssf <youssfbr@gmail.com>
2020-10-05 11:16:11 -0300

    Find Client by id, exception handling

--- src/main/java/com/alissondev/client/services/ClientService.java
+++ src/main/java/com/alissondev/client/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.alissondev.client.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alissondev.client.dto.ClientDTO;
 import com.alissondev.client.entities.Client;
 import com.alissondev.client.repositories.ClientRepository;
+import com.alissondev.client.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -23,4 +25,12 @@ public class ClientService {
 		
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+	
 }
---
+++ src/main/java/com/alissondev/client/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.alissondev.client.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
